af2ac9969041d5a4a7903575139abc4054f18f20
Danilo Almeida <48030619+DaniloAlmeidaSantos@users.noreply.github.com>
2022-03-27 12:47:41 -0300

    Inicializando as classes

--- src/main/java/br/com/edg/marketplace/Application.java
+++ src/main/java/br/com/edg/marketplace/Application.java
@@ -16,6 +16,7 @@ public class Application {
      */
     public static void main(String[] args) {
         FrmLogin login = new FrmLogin();
+        login.setLocationRelativeTo(null);
         login.setVisible(true);
     }
     
