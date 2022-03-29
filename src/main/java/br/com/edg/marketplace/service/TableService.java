e98fdfd9f6cd1f18aab06f95ae69737b8efbb981
DaniloAlmeidaSantos <danilo8almeida@hotmail.com>
2022-03-29 20:37:38 -0300

    Alter service because the teacher not happy for implemetation)

--- src/main/java/br/com/edg/marketplace/service/TableService.java
+++ src/main/java/br/com/edg/marketplace/service/TableService.java
@@ -1,13 +1,33 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
 package br.com.edg.marketplace.service;
+
+import javax.swing.table.DefaultTableModel;
+
 /**
- *
+ * Class for table utils (add, remove, and others)
+ * 
  * @author Danilo
  */
-public interface TableService {
-    void addItem(Object... args);
-    void removeItem(javax.swing.JTable tableUtils);
+public class TableService {
+    
+    private DefaultTableModel defaultTableModel = null;
+    
+    public TableService(javax.swing.JTable  tableUtils) {
+        this.defaultTableModel = (DefaultTableModel) tableUtils.getModel();
+    }
+
+    public void addItem(Object[] args) {
+        try {
+            defaultTableModel.addRow(args);
+        } catch (UnsupportedOperationException e) {
+           throw new UnsupportedOperationException("Not supported yet.");
+        }
+    }
+    
+    public void removeItem(int index) {
+    	try {
+            defaultTableModel.removeRow(index);
+        } catch (UnsupportedOperationException e) {
+           throw new UnsupportedOperationException("Not supported yet.");
+        }
+    }
 }
